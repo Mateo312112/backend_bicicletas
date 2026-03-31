b7c9d29d135d9ff850d69dd8cdc577ee36365908
Mateo312112 <suarezmateo3121@gmail.com>
2026-03-31 17:43:48 -0500

    funcionalidad venta, merge con funcionalidad cliente

---
+++ src/main/java/com/tienda/bicicletas/controller/VentaController.java
@@ -0,0 +1,53 @@
+package com.tienda.bicicletas.controller;
+
+import com.tienda.bicicletas.model.Venta;
+import com.tienda.bicicletas.service.VentaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping
+public class VentaController{
+    @Autowired
+    private VentaService ventaService;
+
+    @PostMapping
+    public Venta registrar(@RequestBody Venta venta)
+    {
+        return  ventaService.registrarVenta(venta);
+    }
+    @GetMapping
+    public List<Venta> listarVentas()
+    {
+        return ventaService.listarVentas();
+    }
+    @DeleteMapping("/{id}")
+    public void eliminarVenta(@PathVariable int id)
+    {
+        ventaService.eliminarVenta(id);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Venta> buscarVentaPorId(@RequestParam int id){
+        return ventaService.buscarVenta(id);
+    }
+
+    @GetMapping("/cliente/{documento}")
+    public List<Venta> buscarPorCliente(@PathVariable int documento){
+        return ventaService.buscarPorCliente(documento);
+    }
+
+    @GetMapping("/{rango}")
+    public List<Venta> buscarPorFechas(
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)LocalDate inicio,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)LocalDate fin){
+
+        return ventaService.buscarPorFechas(inicio, fin);
+    }
+
+}
---
+++ src/main/java/com/tienda/bicicletas/service/VentaService.java
@@ -0,0 +1,47 @@
+package com.tienda.bicicletas.service;
+
+import com.tienda.bicicletas.model.Venta;
+import com.tienda.bicicletas.repository.VentaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+
+public class VentaService
+{
+    @Autowired
+    private VentaRepository ventaRepository;
+
+    public Venta registrarVenta(Venta venta)
+    {
+        return ventaRepository.save(venta);
+    }
+
+    public List<Venta> listarVentas()
+    {
+        return ventaRepository.findAll();
+    }
+
+    public  Optional<Venta> buscarVenta(int idVenta)
+    {
+        return ventaRepository.findById(idVenta);
+    }
+
+    public List<Venta>buscarPorCliente(int documento)
+    {
+        return ventaRepository.findByClienteDocumento(documento);
+    }
+
+    public List<Venta>buscarPorFechas(LocalDate inicio, LocalDate fin)
+    {
+        return ventaRepository.findByFechaVentaBetween(inicio, fin);
+    }
+
+    public void eliminarVenta(int idVenta)
+    {
+        ventaRepository.deleteById(idVenta);
+    }
+}
